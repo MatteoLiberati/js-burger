211170715173db039b52c5ab3c0733f399c246a0
MatteoLiberati <matteoliberati@hotmail.com>
2021-03-26 16:45:41 +0100

    prezzo modificato con scelta ingredienti

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 /*
+***********************
 *ACQUISTO BURGER
+***********************
 *NOME BURGER PERSONALIZZATO OBBLIGATORIO
 *CALCOLO DEL PREZZO BURGER CON LE AGGIUNTE DEGLI INGREDIENTI
 *SPUNTA DEGLI INGREDIENTI MODIFICABILE
@@ -8,19 +10,35 @@
 
 // REFERENCES
 var nameBurger = document.getElementById("name");
-var Ingredients = document.getElementsByClassName("ingredient-checkbox");
+var ingredients = document.getElementsByClassName("ingredient-checkbox");
 var add = document.getElementsByClassName("ingredient-add");
 var discount = document.getElementById("coupon");
 var button = document.getElementById("button");
+var priceDisplay = document.getElementById("price");
+
 
 
 // INTERAZIONE CON CLICK BUTTON
 button.addEventListener("click", function() {
 
     // NOME BURGER PERSONALIZZATO OBBLIGATORIO
-    if(nameBurger.value === ""){
+    if(nameBurger.value.trim() === ""){
         alert("Attenzione, non hai inserito il nome del tuo Burger!");
-    }else{
-        console.log("nomepieno");
+    } else{
+        var price = 50;
+
+        //CONTROLLO CHECK INGREDIENTI
+        for(var i=0; i<ingredients.length; i++){
+            var ingredient = ingredients[i];
+        
+            if(ingredient.checked){
+                console.log("Selezionato " +ingredient.id + " di valore " + ingredient.value);
+                price += parseInt(ingredient.value);
+            }
+            // STAMPA A VIDEO DEL PREZZO AGGIORNATO
+            priceDisplay.innerHTML = price.toFixed(2);
+        }
+
     }
+
 })
