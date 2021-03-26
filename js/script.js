910042e2ccc887a2394fc4c32481f15d9d827e39
MatteoLiberati <matteoliberati@hotmail.com>
2021-03-26 15:37:32 +0100

    references

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*
+*ACQUISTO BURGER
+*NOME BURGER PERSONALIZZATO OBBLIGATORIO
+*CALCOLO DEL PREZZO BURGER CON LE AGGIUNTE DEGLI INGREDIENTI
+*SPUNTA DEGLI INGREDIENTI MODIFICABILE
+*INTERAZIONE CON LE SPUNTE ANCHE DALLO SPAN ADD
+*/
+
+// REFERENCES
+var nameBurger = document.getElementById("name");
+var Ingredients = document.getElementsByClassName("ingredient-checkbox");
+var add = document.getElementsByClassName("ingredient-add");
+var discount = document.getElementById("coupon");
+var button = document.getElementById("button");
